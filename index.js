61cee2cf6b42902663b6151da44f12e8e3725b71
unknown <lesha_minyaev@mail.ru>
2025-05-07 02:37:00 +0300

    Добавил версию для слабовидящих

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Happy developing ✨')
+let baseSize = 16;
+
+function changeFontSize(step) {
+    const newSize = baseSize + step * 2;
+    document.body.style.fontSize = `${newSize}px`;
+}
