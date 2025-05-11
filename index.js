0f5d57ef0eaa43d209e5f3b2c147242d2239c309
unknown <lesha_minyaev@mail.ru>
2025-05-11 15:18:23 +0300

    Добавил функционал кнопки партфолио в хедере, а также форму в левый угол

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function changeFontSize(step) {
     const newSize = baseSize + step * 2;
     document.body.style.fontSize = `${newSize}px`;
 }
+document.addEventListener("DOMContentLoaded", function () {
+    const select = document.getElementById("portfolioSelect");
+    select.addEventListener("change", function () {
+        const url = this.value;
+        if (url) {
+            window.location.href = url;
+        }
+    });
+});
