455877b2c03ca08571b06b482250caf3e4a69f6b
unknown <lesha_minyaev@mail.ru>
2025-05-18 15:15:33 +0300

    Удалил лишнюю кнопку и добавил поиск на все страницы а также создал шаблон страницы преподавателю

--- index.js
+++ index.js
@@ -13,3 +13,27 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+// Пример данных
+const data = {
+    "главная": "/website/index.html",
+    "преподавателю": "/website/Prepod/Prepod.html",
+    "обучающемуся": "/website/Obych/Obych.html",
+    "кнопка": "/website/NO_1/No_1.html",
+    "мое портфолио": "/website/My_Port/My_Port.html",
+    "портфолио учащихся": "/website/Obych_Port/ObychPort.html",
+    "обо мне": "/website/O_Mne/O_Mne.html"
+};
+
+function performSearch() {
+    const query = document.getElementById("searchInput").value.trim().toLowerCase();
+    const errorMessage = document.getElementById("errorMessage");
+
+    if (data.hasOwnProperty(query)) {
+        // Переход на нужную страницу
+        window.location.href = data[query];
+    } else {
+        // Показываем сообщение, если совпадений нет
+        errorMessage.textContent = "Страница по запросу не найдена.";
+    }
+}
+
